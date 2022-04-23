a01b151d820bdd0e02059f143d95bcef27a2186e
TimEconometrics <tim.vandermeer3@gmail.com>
2022-04-23 19:16:08 +0200

    web3hunt 2 dapphunter

--- nextjs/components/organisms/Hero.tsx
+++ nextjs/components/organisms/Hero.tsx
@@ -26,7 +26,7 @@ export const Hero = ({ pageProps }: AppProps) => {
 
           <Paragraph className="mb-8 font-semibold">
             {
-              "We help create the best Web3 user experience. Ping us and let's build something epic together!"
+              "We help you find the best dapps. Ping us and let's build something epic together!"
             }
           </Paragraph>
           <div className="flex justify-center">
