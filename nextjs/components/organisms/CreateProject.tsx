d3e16e9059e7c2517f3249c9ae709a236d6beda3
grmkris <kristjan.grm1@gmail.com>
2022-04-23 17:51:25 +0200

    feat: working project creation

--- nextjs/components/organisms/CreateProject.tsx
+++ nextjs/components/organisms/CreateProject.tsx
@@ -1,6 +1,6 @@
-import React, { useContext, useRef, useState } from 'react';
-import { AppCtx } from '../../context/CtxProvider';
-import { Container } from '../templates/Container';
+import React, {useContext, useRef, useState} from 'react';
+import {AppCtx} from '../../context/CtxProvider';
+import {Container} from '../templates/Container';
 import {
   useAccount,
   useContract,
@@ -8,19 +8,19 @@ import {
   useSigner,
   useContractWrite,
 } from 'wagmi';
-import { PrimaryButton } from '../atoms/Buttons';
+import {PrimaryButton} from '../atoms/Buttons';
 import base58 from 'bs58';
 import {
   CMSAction,
   WEB3_HUNT_CONTRACT,
   WEB3_HUNT_WEBSITE_RINKEBY,
 } from '../../constants/api.const';
-import { WEB3HUNT_ABI } from '../../abis/Web3HuntContentManager';
-import { Web3HuntContentManager } from '../../types/Web3HuntContentManager';
-import { ethers } from 'ethers';
+import {WEB3HUNT_ABI} from '../../abis/Web3HuntContentManager';
+import {Web3HuntContentManager} from '../../types/Web3HuntContentManager';
+import {ethers} from 'ethers';
 
 export function CreateProject() {
-  const [{ data: accountData }] = useAccount({
+  const [{data: accountData}] = useAccount({
     fetchEns: false,
   });
   const signer = useSigner();
@@ -148,10 +148,28 @@ export function CreateProject() {
     let mediaCIDs = [];
 
     for (let m of media) {
-      const previewRes = await fetch('/api/upload', {
+      const previewRes = await fetch(
+        '/api/upload',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            fileName: 'picture',
+            payload: await getBase64(m)
+          })
+        }
+      )
+      mediaCIDs.push(await previewRes.text())
+    }
+
+    const res = await fetch(
+      '/api/ipfs-json',
+      {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify({
           projectBody: {
@@ -161,176 +179,178 @@ export function CreateProject() {
             tags: tags,
             imagePreview: imagePreviewCID,
             media: mediaCIDs,
-            metadata_type: 'project_metadata',
-          },
-        }),
-      });
+            metadata_type: "project_metadata"
+          }
+        })
+      }
+    )
 
-      if (!res.ok) throw new Error('Error creating project');
-      const result = await res.json();
-      alert(result.IpfsHash);
-      const onchainResult = await createProjectOnchain(
-        result.IpfsHash,
-        WEB3_HUNT_WEBSITE_RINKEBY
-      );
-      console.log('onchainResult', onchainResult);
-      console.log(result);
-      // result.user => 'Ada Lovelace'
-    }
-
-    return (
-      <div className="fixed z-20 w-full h-full bg-[#0202029e] backdrop-blur top-0">
-        <Container className="justify-center items-center h-full">
-          <form
-            onSubmit={createProject}
-            className="w-full bg-zinc-50 p-8 md:p-24 rounded-xl relative animate__animated animate__fadeInUpBig"
-          >
-            <button
-              className="absolute top-10 right-0 z-50 inline-flex items-center justify-center rounded-md px-4 py-4 text-zinc-800 hover:text-gray-300 focus:outline-none md:right-10 md:px-4"
-              onClick={() => ctx?.modalCtx.dispatch({ type: 'close' })}
-            >
-              <div className="flex w-8 flex-col items-end">
-                <span className="block h-1 w-full translate-y-1 -rotate-45 rounded-full bg-zinc-800 transition-all"></span>
-                <span className="block h-1 w-full rotate-45 rounded-full bg-zinc-800 transition-all"></span>
-              </div>
-            </button>
-            <div className="group relative z-0 mb-6 w-full">
-              <label
-                htmlFor="name"
-                className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-zinc-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-purple-600  "
-              >
-                Name*
-              </label>
-              <input
-                type="text"
-                {...{
-                  required: {
-                    value: true,
-                    message: 'Please enter the project name',
-                  },
-                }}
-                id="name"
-                className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-zinc-900 focus:border-purple-600 focus:outline-none focus:ring-0 "
-                placeholder="EthGlobal"
-                required
-              />
-            </div>
-            <div className="group relative z-0 mb-6 w-full">
-              <label
-                htmlFor="short_descriptionn"
-                className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-zinc-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-purple-600 "
-              >
-                Short description*
-              </label>
-              <input
-                type="text"
-                {...{
-                  required: {
-                    value: true,
-                    message: 'Please enter a short project description',
-                  },
-                }}
-                id="short_description"
-                className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-zinc-900 focus:border-purple-600 focus:outline-none focus:ring-0 "
-                placeholder="EthGlobal demo project"
-                required
-              />
-            </div>
-            <div className="group relative z-0 mb-6 w-full">
-              <label
-                htmlFor="description"
-                className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-zinc-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-purple-600 "
-              >
-                Description*
-              </label>
-              <input
-                type="text"
-                {...{
-                  required: {
-                    value: true,
-                    message: 'Please enter a full project description',
-                  },
-                }}
-                id="description"
-                className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-zinc-900 focus:border-purple-600 focus:outline-none focus:ring-0 "
-                placeholder="This is the best application ever, it codes itself and does everything you will ever need"
-                required
-              />
-            </div>
-            <div className="group relative z-0 mb-6 w-full">
-              <label
-                htmlFor="tags"
-                className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-zinc-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-purple-600 "
-              >
-                Tags*
-              </label>
-              <input
-                type="text"
-                {...{
-                  required: {
-                    value: true,
-                    message: 'Please enter tags for your project',
-                  },
-                }}
-                id="tags"
-                className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-zinc-900 focus:border-purple-600 focus:outline-none focus:ring-0 "
-                placeholder="ethglobal, ADASD"
-                required
-              />
-            </div>
-            <div className="group relative z-0 mb-6 w-full">
-              <input
-                ref={filePickerRef}
-                className="hidden"
-                type="file"
-                accept=".jpg, .png, .jpeg"
-                onChange={pickedHandler}
-              />
-              <div
-                className={`m-auto flex max-w-md items-center rounded-lg bg-gray-50 p-5 shadow-2xl `}
-                onClick={pickImageHandler}
-              >
-                <div className="flex w-full flex-col items-center justify-center space-y-2">
-                  {!imagePreview ? (
-                    <p>
-                      <p className="w-full text-center">Upload preview image</p>
-                    </p>
-                  ) : (
-                    <p>{imagePreview.name}</p>
-                  )}
-                </div>
-              </div>
-            </div>
-            <div className="group relative z-0 mb-6 w-full">
-              <input
-                ref={filePickerRefMedia}
-                className="hidden"
-                type="file"
-                accept=".jpg, .png, .jpeg"
-                onChange={pickedHandlerMedia}
-              />
-              <div
-                className={`m-auto flex max-w-md items-center rounded-lg bg-gray-50 p-5 shadow-2xl`}
-                onClick={pickImageHandlerMedia}
-              >
-                <div className="flex w-full flex-col items-center justify-center space-y-2">
-                  <p>
-                    <p className="w-full text-center">Add media pictures</p>
-                    <ul>
-                      {media.map((value, index) => {
-                        return <li key={index}>{value.name}</li>;
-                      })}
-                    </ul>
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="group relative z-0 mb-6 w-full flex justify-center">
-              <PrimaryButton type="submit">Create Project</PrimaryButton>
-            </div>
-          </form>
-        </Container>
-      </div>
+    if (!res.ok) throw new Error('Error creating project');
+    const result = await res.json();
+    alert(result.IpfsHash);
+    const onchainResult = await createProjectOnchain(
+      result.IpfsHash,
+      WEB3_HUNT_WEBSITE_RINKEBY
     );
-  };
+    console.log('onchainResult', onchainResult);
+    console.log(result);
+    // result.user => 'Ada Lovelace'
+  }
+
+  return (
+    <div className="fixed z-20 w-full h-full bg-[#0202029e] backdrop-blur top-0">
+      <Container className="justify-center items-center h-full">
+        <form
+          onSubmit={createProject}
+          className="w-full bg-zinc-50 p-8 md:p-24 rounded-xl relative animate__animated animate__fadeInUpBig"
+        >
+          <button
+            className="absolute top-10 right-0 z-50 inline-flex items-center justify-center rounded-md px-4 py-4 text-zinc-800 hover:text-gray-300 focus:outline-none md:right-10 md:px-4"
+            onClick={() => ctx?.modalCtx.dispatch({type: 'close'})}
+          >
+            <div className="flex w-8 flex-col items-end">
+              <span
+                className="block h-1 w-full translate-y-1 -rotate-45 rounded-full bg-zinc-800 transition-all"></span>
+              <span className="block h-1 w-full rotate-45 rounded-full bg-zinc-800 transition-all"></span>
+            </div>
+          </button>
+          <div className="group relative z-0 mb-6 w-full">
+            <label
+              htmlFor="name"
+              className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-zinc-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-purple-600  "
+            >
+              Name*
+            </label>
+            <input
+              type="text"
+              {...{
+                required: {
+                  value: true,
+                  message: 'Please enter the project name',
+                },
+              }}
+              id="name"
+              className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-zinc-900 focus:border-purple-600 focus:outline-none focus:ring-0 "
+              placeholder="EthGlobal"
+              required
+            />
+          </div>
+          <div className="group relative z-0 mb-6 w-full">
+            <label
+              htmlFor="short_descriptionn"
+              className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-zinc-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-purple-600 "
+            >
+              Short description*
+            </label>
+            <input
+              type="text"
+              {...{
+                required: {
+                  value: true,
+                  message: 'Please enter a short project description',
+                },
+              }}
+              id="short_description"
+              className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-zinc-900 focus:border-purple-600 focus:outline-none focus:ring-0 "
+              placeholder="EthGlobal demo project"
+              required
+            />
+          </div>
+          <div className="group relative z-0 mb-6 w-full">
+            <label
+              htmlFor="description"
+              className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-zinc-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-purple-600 "
+            >
+              Description*
+            </label>
+            <input
+              type="text"
+              {...{
+                required: {
+                  value: true,
+                  message: 'Please enter a full project description',
+                },
+              }}
+              id="description"
+              className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-zinc-900 focus:border-purple-600 focus:outline-none focus:ring-0 "
+              placeholder="This is the best application ever, it codes itself and does everything you will ever need"
+              required
+            />
+          </div>
+          <div className="group relative z-0 mb-6 w-full">
+            <label
+              htmlFor="tags"
+              className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-zinc-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-purple-600 "
+            >
+              Tags*
+            </label>
+            <input
+              type="text"
+              {...{
+                required: {
+                  value: true,
+                  message: 'Please enter tags for your project',
+                },
+              }}
+              id="tags"
+              className="peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent py-2.5 px-0 text-sm text-zinc-900 focus:border-purple-600 focus:outline-none focus:ring-0 "
+              placeholder="ethglobal, ADASD"
+              required
+            />
+          </div>
+          <div className="group relative z-0 mb-6 w-full">
+            <input
+              ref={filePickerRef}
+              className="hidden"
+              type="file"
+              accept=".jpg, .png, .jpeg"
+              onChange={pickedHandler}
+            />
+            <div
+              className={`m-auto flex max-w-md items-center rounded-lg bg-gray-50 p-5 shadow-2xl `}
+              onClick={pickImageHandler}
+            >
+              <div className="flex w-full flex-col items-center justify-center space-y-2">
+                {!imagePreview ? (
+                  <p>
+                    <p className="w-full text-center">Upload preview image</p>
+                  </p>
+                ) : (
+                  <p>{imagePreview.name}</p>
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="group relative z-0 mb-6 w-full">
+            <input
+              ref={filePickerRefMedia}
+              className="hidden"
+              type="file"
+              accept=".jpg, .png, .jpeg"
+              onChange={pickedHandlerMedia}
+            />
+            <div
+              className={`m-auto flex max-w-md items-center rounded-lg bg-gray-50 p-5 shadow-2xl`}
+              onClick={pickImageHandlerMedia}
+            >
+              <div className="flex w-full flex-col items-center justify-center space-y-2">
+                <p>
+                  <p className="w-full text-center">Add media pictures</p>
+                  <ul>
+                    {media.map((value, index) => {
+                      return <li key={index}>{value.name}</li>;
+                    })}
+                  </ul>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="group relative z-0 mb-6 w-full flex justify-center">
+            <PrimaryButton type="submit">Create Project</PrimaryButton>
+          </div>
+        </form>
+      </Container>
+    </div>
+  )
 }
+
