4f2d2728daab9707b325281d1b953834704b2824
TimEconometrics <tim.vandermeer3@gmail.com>
2022-04-23 12:44:54 +0200

    add filter

---
+++ nextjs/components/Filter.tsx
@@ -0,0 +1,24 @@
+import { Project } from "../constants/placeholders"
+
+export enum Categories {
+    DEFI = 'Defi',
+    NFT = 'Nft',
+    HOT = 'Hot',
+    YOURPROJECTS = 'Your projects'
+  }
+
+function getAllProjects() {
+    return []
+}
+
+export function selectCategory(category: Categories){
+    var projectsInCategory: Array<Project> = [];
+    var AllProjects: Array<Project> = getAllProjects()
+    AllProjects.forEach( (project) => {
+        if (project.tags.includes(category)){
+            projectsInCategory.push(project);
+        }
+    })
+    return projectsInCategory
+}
+
